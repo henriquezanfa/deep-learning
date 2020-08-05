e6976b2aa35598155ea6e6a953433b01b242902a
Henrique Hadermeck <henriquezanferrari@gmail.com>
2020-08-04 21:39:23 -0300

    Implementação da validação cruzada

---
+++ Cancer de Mama/cancer_mama_cruzada.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+
+previsores = pd.read_csv('entradas_breast.csv')
+classe = pd.read_csv('saidas_breast.csv')
+
+def criarRede():
+    classificador = Sequential()
+    
+    classificador.add(Dense(units = 16, activation = 'relu', kernel_initializer='random_uniform', input_dim = 30))
+    classificador.add(Dense(units = 16, activation = 'relu', kernel_initializer='random_uniform'))
+    classificador.add(Dense(units = 1, activation= 'sigmoid'))
+
+    otimizador = keras.optimizers.Adam(lr = 0.001, decay=0.0001)
+    
+    classificador.compile(optimizer = otimizador, loss = 'binary_crossentropy', metrics= ['binary_accuracy'])
+    
+    return classificador
+
+classificador = KerasClassifier(build_fn=criarRede, 
+                                epochs = 100, 
+                                batch_size = 10)
+
+# cv => numero de validações cruzadas
+resultados = cross_val_score(estimator = classificador, 
+                             X = previsores,
+                             y = classe, 
+                             cv = 10, 
+                             scoring='accuracy')
+
+# media dos resultados da rede
+media = resultados.mean()
+
+# desvio padrao
+desvio = resultados.std()
